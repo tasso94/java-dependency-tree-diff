d6f0a60cf1cad1fa3c048a677dae220a5eab8e32
Tassilo Weidner <tassilo.weidner@camunda.com>
2021-04-09 23:35:07 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,14 +9,13 @@ if [[ $(git diff origin/master HEAD --name-only | grep pom.xml$ | wc -c) -ne 0 ]
     apt install -y nodejs npm rsync
 
     cd /github/workspace
-
-    #mvn -T 16C clean package -DskipTests -Dskip-third-party-bom=false -Dthird-party-bom-scopes="compile|provided|runtime|test"
-    mvn -T 8C org.codehaus.mojo:license-maven-plugin:2.0.0:aggregate-download-licenses -Dlicense.excludedScopes=test,compile,runtime,provided -Dlicense.excludedGroups="org\.camunda.*"
-    find . -name 'licenses.xml' -exec rsync -R \{\} /pr \;
+    
+    mvn -T 8C org.codehaus.mojo:license-maven-plugin:2.0.0:aggregate-add-third-party -Dlicense.includedScopes=test,compile,runtime,provided -Dlicense.excludedGroups="org\.camunda.*"
+    find . -name 'THIRD-PARTY.txt' -exec rsync -R \{\} /pr \;
 
     git checkout -f origin/master
-    mvn -T 8C org.codehaus.mojo:license-maven-plugin:2.0.0:aggregate-download-licenses -Dlicense.excludedScopes=test,compile,runtime,provided -Dlicense.excludedGroups="org\.camunda.*"
-    find . -name 'licenses.xml' -exec rsync -R \{\} /master \;
+    mvn -T 8C org.codehaus.mojo:license-maven-plugin:2.0.0:aggregate-add-third-party -Dlicense.includedScopes=test,compile,runtime,provided -Dlicense.excludedGroups="org\.camunda.*""
+    find . -name 'THIRD-PARTY.txt' -exec rsync -R \{\} /master \;
 
     echo -e "<details><summary>Dependency Tree Diff</summary><p>\n" >/github/workspace/dep-tree-diff.txt
     echo "\`\`\`diff" >>/github/workspace/dep-tree-diff.txt
